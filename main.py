5cc7d05fd5f922afee1727fc7ee8cc6d1714fabb
Gaurav Raj Singh <grsmanohar@gmail.com>
2024-09-12 12:45:45 +0530

    function to create a new user account created and tested

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import requests
+import dotenv
+import os
+
+# load dotenv.
+dotenv.load_dotenv()
+
+
+def create_user_account():
+
+    params = {
+        "token": os.getenv('create_user_account_token'),
+        "username": os.getenv('create_user_account_username'),
+        "agreeTermsOfService": "yes",
+        "notMinor": "yes"
+    }
+
+    response = requests.post(url= os.getenv('create_user_account_end_point'), json=params)
+    print(response.status_code)
+
+# create a new user
+# create_user_account()
+
